666948aaa43b791ea3ca4d81a9c43a38b9f7d0f6
sonuzero <thebraindrop.zero@gmail.com>
2024-06-01 09:55:56 +0530

    store bug 2

--- pages/ProfilePage.tsx
+++ pages/ProfilePage.tsx
@@ -15,7 +15,7 @@ interface ProfilePageProps {
 }
 
 const ProfilePage: React.FC<ProfilePageProps> = ({ }) => {
-    const user = useSelector((state: RootState) => state.user.user);
+    const user = useSelector((state: RootState) => state?.user.user);
     return (
         <>
             {
